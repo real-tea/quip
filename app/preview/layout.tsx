7b75986472dfa27a636c4cedd298b2469327a53c
real-tea <akash.0711a@gmail.com>
2023-12-17 17:44:34 +0530

    document id for each page

---
+++ app/preview/layout.tsx
@@ -0,0 +1,9 @@
+import { ChildProps } from "@/types";
+import React from "react";
+
+const Layout = ({children}:ChildProps)=>{
+    return <div className = "h-full">{children}</div>
+}
+
+
+export default Layout;
